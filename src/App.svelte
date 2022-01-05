b20219814716de82f19d3e22e6b463c64ef60904
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-01-05 10:26:24 +0100

    There's a fucking donut!

--- src/App.svelte
+++ src/App.svelte
@@ -1,60 +1,45 @@
 <script>
 import { onMount } from "svelte";
+import * as THREE from 'three';
 
-	let todos = [];
-	let toDoInput = "";
 
-	function addToDo(newToDo) {
-		todos = [...todos, newToDo];
-		localStorage.setItem("todos", JSON.stringify(todos))
-	}
-
-	function removeToDo(id) {
-		console.log("test");
-		
-		todos.splice(id, 1);
-		todos = todos;
-	}
 
 	onMount(() => {
-		todos = JSON.parse(localStorage.getItem("todos")) || [];
+		//Decaling a new scene object
+		const scene = new THREE.Scene();
+
+		//Defining camera and propeties
+		const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+		const renderer = new THREE.WebGL1Renderer({
+			canvas: document.querySelector('#model')
+		});
+
+		renderer.setPixelRatio( window.devicePixelRatio );
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		camera.position.setZ(30);
+
+		renderer.render( scene, camera );
+
+		const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+		const material = new THREE.MeshBasicMaterial( {color: 0xFF6347, wireframe: true } );
+		const torus = new THREE.Mesh( geometry, material );
+
+		scene.add(torus);
+
+		function animate() {
+			requestAnimationFrame( animate );
+			renderer.render( scene, camera );
+		}
+
+		animate();
+		
 	})
 
 </script>
 
 <main>
-	<h1>To Do App</h1>
-	<form on:submit|preventDefault={addToDo(toDoInput)}>
-		<input type="text" bind:value="{toDoInput}" placeholder="Write new todo here">
-		<button type="submit" >Add</button>
-	</form>
-
-	<ul style="list-style-type: none;">
-		{#each todos as todoItem, i}
-		<button on:click|once="{removeToDo(i)}"><li>{todoItem}</li></button> <br>
-		{/each}
-	</ul>
-	<button on:click|once="{removeToDo}"><li>test</li></button> <br>
+	<h1>3D view</h1>
+	<canvas id="model"></canvas>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
