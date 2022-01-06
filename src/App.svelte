25e0a09154697c89757991e5e3b3eb521f70ec40
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-01-06 07:06:15 +0100

    Added interactivity

--- src/App.svelte
+++ src/App.svelte
@@ -2,51 +2,61 @@
 import { onMount } from "svelte";
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 //Decaling a new scene object
 const scene = new THREE.Scene();
 
 //Defining camera and propeties
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.setZ(3);
 
 // Geometry
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshBasicMaterial( {color: 0xFF6347, wireframe: true } );
 const torus = new THREE.Mesh( geometry, material );
+//scene.add(torus);
 
-//Add light
-const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+
+//Add ambient light
+const light = new THREE.AmbientLight( 0x404040, 2 ); // soft white light
 scene.add( light );
 
+//Add directional light
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 5 );
+directionalLight.position.set(0,1,0);
+directionalLight.castShadow = true;
+scene.add(directionalLight);
 
-
+//Load custom object
 const loader = new GLTFLoader();
-
 loader.load( 'assets/test_model.glb', function ( gltf ) {
-
-	console.log("Added model to scene");
 	scene.add( gltf.scene );
-
 }, undefined, function ( error ) {
-
 	console.error( error );
-
 } );
 
-//scene.add(torus);
 
 onMount(() => {
 
 	//On mount connect render to canvas element
-	const renderer = new THREE.WebGL1Renderer({
+	// const renderer = new THREE.WebGL1Renderer({
+	// 	canvas: document.querySelector('#model')
+	// });
+	const renderer = new THREE.WebGLRenderer({ 
+		antialias: true,
 		canvas: document.querySelector('#model')
-	});
+	 });
+
+	 //Interactive
+	 const controls = new OrbitControls( camera,  renderer.domElement);
+	controls.addEventListener('mousechange', renderer);
 
 	//Set size and pixel ratio
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	
+	 
 	//Define clock from three.js
 	const clock = new THREE.Clock();
 
@@ -56,7 +66,7 @@ onMount(() => {
 		const elapsedTime = clock.getElapsedTime();
 
 		//Define animation relative to time between ticks
-		torus.rotation.x = 0.8 * elapsedTime;
+		torus.rotation.x = 0.8 *1;
 		torus.rotation.y = 1 * elapsedTime;
 		torus.rotation.z = 1.2 * elapsedTime;
 
