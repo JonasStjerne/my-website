4a8d71b1c3bdfb8584475f6fdf746f053fe49289
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-01-29 12:47:00 +0100

    Fuck backwards for css animations. Works like dogshit pls fix

--- src/ProjectCard.svelte
+++ src/ProjectCard.svelte
@@ -31,35 +31,109 @@
         }
     ]; 
     let selectedProject = 0;
-    let projectImageElement;
+    let currentProjectImage, projectImageContainer;
     $ : percentProjectBar = ( (selectedProject + 1) / projects.length ) * 100;
 
+    function removeAfterAnimation(elm) {
+        elm.addEventListener('animationend', (e) => {
+                elm.remove()
+                console.log("removed");
+                console.log(elm);            
+		}, { once: true });
+    }
+
+    function slideImage(side, inOrOut, elm) {
+        //remove existing animation classes
+        elm.classList.remove('c_slideInRight');
+        elm.classList.remove('c_slideInLeft');
+
+        if (side =="left") {
+            switch (inOrOut) {
+                case "in":
+                    elm.classList.add("c_slideInLeft")
+                    currentProjectImage = elm;
+                break;
+                case "out":
+                    removeAfterAnimation(elm);
+                    elm.classList.add("c_slideOutLeft")
+                break;
+            }
+        } else {
+            switch (inOrOut) {
+                case "in":
+                    elm.classList.add("c_slideInRight")
+                    currentProjectImage = elm;
+                break;
+                case "out":
+                    removeAfterAnimation(elm);
+                    elm.classList.add("c_slideOutRight")
+                break;
+            }
+        }
+    }
+    
+
+    function createNewImageElement() {
+        let imageElement = document.createElement("img");
+        imageElement.classList.add("h-50", "position-absolute", "top-50", "shadowDarker", "p-0", "projectImage");
+        imageElement.style.cssText = 'transform: translateY(-50%); border-radius: 8px;';
+        return imageElement;
+    }
 
     function nextProject() {
+
         if (selectedProject < projects.length - 1) {
-            projectImageElement.classList.add('slideOutLeft');
+            //Increment to new project index
+            selectedProject++;
+
+            //Slide out current image
+           slideImage("left", "out", currentProjectImage);
+
+            //Create new image for the next project
+            let newImageElement = createNewImageElement();
+            newImageElement.src = projects[selectedProject].src;
+
+            //Add the new image to the dom. The animation will automatically trigger
+            projectImageContainer.appendChild(newImageElement);
+            slideImage("right", "in", newImageElement)
+          
         }
     }
 
     function prevProject() {
-        if (selectedProject > 0) selectedProject--;
+        if (selectedProject > 0) {
+            //Increment to new project index
+            selectedProject--;
+
+            //Slide out current image
+           slideImage("right", "out", currentProjectImage);
+
+            //Create new image for the next project
+            let newImageElement = createNewImageElement();
+            newImageElement.src = projects[selectedProject].src;
+
+            //Add the new image to the dom. The animation will automatically trigger
+            projectImageContainer.appendChild(newImageElement);
+            slideImage("left", "in", newImageElement)
+          
+        }
     }
 
     onMount(() => {
-        projectImageElement = document.querySelector('.projectImage');
+        currentProjectImage = document.querySelector('.projectImage');
     })
 
 </script>
 
 <div class="mt-5 d-flex justify-content-center">
     <div class="position-relative container m-0 col-8" style="height:0;width:30%;padding-bottom:30%;">
-        <div class="position-absolute w-100 h-100 d-flex flex-column justify-content-between p-2 top-0 left-0" >
+        <div class="position-absolute w-100 h-100 d-flex flex-column justify-content-between p-2 top-0 left-0 position-relative" >
             <div class="p-4">
                 <h3 class="row text-white pb-2">{projects[selectedProject].title}</h3>
-                <div class="row positon-relative">
+                <div class="row" bind:this={projectImageContainer}>
                     <p class="col-7 p-0 text-white">{projects[selectedProject].text}</p>
                     <div class="col-5"></div>
-                    <img class="h-50 position-absolute top-50 shadowDarker p-0 projectImage" style="transform: translateY(-50%); border-radius: 8px;" src="{projects[selectedProject].src}" alt="">
+                    <img class="h-50 position-absolute top-50 shadowDarker p-0 projectImage c_slideInLeft" id="0" style="transform: translateY(-50%); border-radius: 8px;" src="{projects[selectedProject].src}" alt="">
                 </div>
                 <svg class="position-absolute w-100 h-100" style="top: 0; left: 0; z-index: -1; border-radius: 3%;" id="visual" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"><defs><filter id="blur1" x="-10%" y="-10%" width="120%" height="120%"><feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood><feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"></feBlend><feGaussianBlur stdDeviation="161" result="effect1_foregroundBlur"></feGaussianBlur></filter></defs><rect width="900" height="600" fill="#e800ff"></rect><g filter="url(#blur1)"><circle cx="877" cy="11" fill="#ff9b00" r="357"></circle><circle cx="259" cy="45" fill="#e800ff" r="357"></circle><circle cx="866" cy="592" fill="#ff9b00" r="357"></circle><circle cx="743" cy="302" fill="#ff9b00" r="357"></circle><circle cx="288" cy="498" fill="#e800ff" r="357"></circle><circle cx="192" cy="260" fill="#ff9b00" r="357"></circle></g></svg>
             </div>
