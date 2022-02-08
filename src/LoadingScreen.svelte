ded191ed71f7cc320f510af15248fc036fc1b9ac
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-02-08 22:52:40 +0100

    animation work

--- src/LoadingScreen.svelte
+++ src/LoadingScreen.svelte
@@ -1,24 +1,62 @@
 <script>
-
+    export let loaded;
 </script>
 
-<div class="position-fixed" style="width: 100vw; height: 100vw; top: 0; left: 0; background-color: white; z-index: 10;">
-    <div style="background-color: #171717; position: absolute; bottom: 0; width:100%; z-index:10;" id="fadeUpBaground"></div>
-    <img src="assets/jsLogoWhite.png" alt="Logo" style="z-index: 20; position: absolute; left: 50%; top:50%; transform: translate(-50%,-50%); min-width: 100px; width: 15vw;">
+<div class="position-fixed {loaded === true ? 'dissapear' : ''}" style="width: 100vw; height: 100vw; top: 0; left: 0; z-index: 20;">
+    <div class="w-100 h-100 d-flex justify-content-center align-items-center" style="background-color: {loaded === true ? 'transparent' : 'white'}; z-index: 10;">
+        <img src="assets/jsLogoWhite.png" alt="Logo" style="min-width: 100px; width: 15vw; postion relative; margin: auto; z-index: 20; animation-fill-mode: forwards;" class="{loaded === true ? 'animate__bounceOut' : '' }">
+    </div>
+
+    <div style="background-color: #171717; position: absolute; width:100%; z-index:10;" class="{loaded === true ? 'loadDone' : 'fadeUpBaground' }">
+    </div>
 </div>
 
 <style>
-    #fadeUpBaground {
-        animation: fillOut 2s ease-out forwards 1s;
+    .fadeUpBaground {
+        animation: fillOut 2s ease-in-out forwards 0.5s;
+        bottom: 0;
     }
+
+    .loadDone {
+        animation: foldUp 1s ease-in-out forwards;
+        bottom: unset;
+        top: 0;
+    }
+
+    .dissapear {
+        animation: dissapear 0.1s ease forwards 1s;
+    }
+
 
     @keyframes fillOut {
         0% {
-            height: 0;
+            height: 0%;
         }
 
         100% {
             height: 100%;
         }
     }
+
+    @keyframes foldUp {
+        0% {
+            height: 100%;
+        }
+
+        100% {
+            height: 0%;
+        }
+    }
+
+    @keyframes dissapear {
+        0% {
+            top: 0;
+        }
+
+        100% {
+            top: -100vh;
+        }
+    }
+    
+
 </style>
